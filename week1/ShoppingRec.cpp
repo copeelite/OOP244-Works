d22ddaee4845f33d10f9a92a051a6b0d472a399b
FanghaoMeng <fmeng18@myseneca.ca>
2022-05-19 16:54:03 -0400

    renew

--- week1/ShoppingRec.cpp
+++ week1/ShoppingRec.cpp
@@ -1,3 +1,19 @@
+/*
+*****************************************************************************
+						Assignment 1 - Workshop1Part1
+Full Name  :FanghaoMeng
+Student ID#:180210213
+Email      :fmeng18@myseneca.ca
+Section    :OOP244NAA
+
+Authenticity Declaration:
+I have done all the coding by myself and only copied the code that my professor provided
+to complete my workshops and assignments.
+I declare this submission is the result of my own work and has not been
+shared with any other student or 3rd party content provider. This submitted
+piece of work is entirely of my own creation.
+*****************************************************************************
+*/
 #define _CRT_SECURE_NO_WARNINGS
 #include <cstdio>
 #include <iostream>
